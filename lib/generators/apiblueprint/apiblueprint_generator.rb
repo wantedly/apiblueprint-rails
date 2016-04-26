7b756ac4f016cfdf012a0214754cc2fa41d1a7d4
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-04-26 16:10:25 +0900

    Move generators to rails namespace

--- lib/generators/apiblueprint/apiblueprint_generator.rb
+++
@@ -1,3 +0,0 @@
-class ApiblueprintGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
-end
---
+++ lib/generators/rails/apiblueprint/apiblueprint_generator.rb
@@ -0,0 +1,3 @@
+class Rails::ApiblueprintGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+end
