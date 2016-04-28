60b41635a7b12492f450f36aa20012b8b6a3dde8
Go Takagi <takagi@shimastripe.com>
2016-04-28 11:35:23 +0900

    Modify argument init

--- lib/generators/rails/apiblueprint/apiblueprint_generator.rb
+++ lib/generators/rails/apiblueprint/apiblueprint_generator.rb
@@ -14,23 +14,19 @@ class Rails::ApiblueprintGenerator < Rails::Generators::NamedBase
   def set_attribute
     @attrs = Array.new()
     attributes.map do |val|
-      @attrs.push( { name: val.name, type: val.type } )
+      @attrs.push( { name: val.name, type: val.type, init: val.default } )
     end
     @attrs.map do |attr|
       case attr[:type].to_s
       when "integer", "primary_key", "decimal", "float" then
         attr[:type] = "number"
-        attr[:init] = 1
       when "boolean" then
         attr[:type] = "boolean"
-        attr[:init] = true
       when "references" then
         attr[:name] = attr[:name] + "_id"
         attr[:type] = "number, required"
-        attr[:init] = 1
       else
         attr[:type] = "string"
-        attr[:init] = (attr[:name].upcase).gsub("_", "")
       end
     end
   end
